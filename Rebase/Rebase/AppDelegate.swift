bb21a8417547ff19026cf18d9b11eb2cd69d8f32
Rodrigo Larrosa <rlarrosa@costar.com>
2016-06-03 10:39:14 -0400

    Dev A - 2 and 3

--- Rebase/Rebase/AppDelegate.swift
+++ Rebase/Rebase/AppDelegate.swift
@@ -32,10 +32,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillEnterForeground(application: UIApplication) {
         // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+        //2
     }
 
     func applicationDidBecomeActive(application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        //3
     }
 
     func applicationWillTerminate(application: UIApplication) {
